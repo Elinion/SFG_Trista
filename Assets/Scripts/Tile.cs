b6ae760f0dd67396e13e3760b20dadf9a8d52e46
alexandre <alexandre.abts@gmail.com>
2017-07-26 13:44:19 +0400

    created tile with 8 possible colors

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+
+	public enum Color
+	{
+		Black,
+		Blue,
+		Green,
+		Orange,
+		Purple,
+		Red,
+		White,
+		Yellow
+	}
+
+	public Texture black;
+	public Texture blue;
+	public Texture green;
+	public Texture orange;
+	public Texture purple;
+	public Texture red;
+	public Texture white;
+	public Texture yellow;
+}
